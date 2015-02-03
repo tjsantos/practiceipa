7597a477f129af34acb6bf14e9514ac946e004d6
T.J. Santos <tjscode@outlook.com>
2015-02-03 13:44:29 -0800

    Register models in admin

--- ipa/admin.py
+++ ipa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ipa.models import Entry, Pronunciation
 
 # Register your models here.
+
+class PronunciationInline(admin.StackedInline):
+    model = Pronunciation
+
+class EntryAdmin(admin.ModelAdmin):
+    inlines = [PronunciationInline]
+
+admin.site.register(Entry, EntryAdmin)
