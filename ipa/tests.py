ec77db615369bbf5536b6f9de6a3314ec3fc44eb
T.J. Santos <tjscode@outlook.com>
2015-02-05 16:33:19 -0800

    Add basic test for detail view

--- ipa/tests.py
+++ ipa/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from ipa.models import Entry, Pronunciation
 
 # Create your tests here.
+
+class EntryViewTest(TestCase):
+
+    def test_can_see_pronunciation_details(self):
+        word = 'test'
+        lang = 'en'
+        ipas = ['ipa1', 'ipa2']
+        entry = Entry.objects.create(entry=word)
+        for ipa in ipas:
+            Pronunciation.objects.create(ipa=ipa, entry=entry)
+
+        response = self.client.get(reverse('detail', args=(lang, word)))
+        self.assertContains(response, word)
+        for ipa in ipas:
+            self.assertContains(response, ipa)
