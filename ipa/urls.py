0b2b0e16be94632ed4c0abd04b5cf38ae1ea021a
T.J. Santos <tjscode@outlook.com>
2015-02-17 00:08:35 -0800

    DRY lang param in urls

--- ipa/urls.py
+++ ipa/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
     url(r'^$', 'ipa.views.index', name='index'),
-    url(r'^(\w+)/(\w+)$', 'ipa.views.detail', name='detail'),
+    url(r'^(?P<lang>\w+)/', include(patterns('',
+        url(r'^(?P<search>\w+)$', 'ipa.views.detail', name='detail'),
+    ))),
     url(r'^search$', 'ipa.views.search', name='search')
 )
