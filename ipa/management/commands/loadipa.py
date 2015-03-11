9fb2fa87edd17f803029ff1611d4b14c055319e7
T.J. Santos <tjscode@outlook.com>
2015-03-11 06:21:37 -0700

    Fix: delete audiofiles on ipa sync

--- ipa/management/commands/loadipa.py
+++ ipa/management/commands/loadipa.py
@@ -14,18 +14,24 @@ class Command(BaseCommand):
             with open(filename, 'r', encoding='utf-8') as f:
                 more_json_words = json.load(f)
             json_words.update(more_json_words)
-        #import sys
-        #import codecs
-        #sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+        import sys
+        import codecs
+        sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
         db_words = Word.objects.all()
 
         # delete words not present in load file
         for word_obj in db_words:
             if word_obj.word not in json_words:
+                print('deleting {}'.format(word_obj));
+                # call custom delete method to delete associated audio files
+                for audio in word_obj.audio_set.all():
+                    audio.delete()
                 word_obj.delete()
 
         for word, info in json_words.items():
             word_obj, created = Word.objects.get_or_create(word=word)
+            if created:
+                print('created {}'.format(word_obj))
             if 'ipa' in info:
                 ipas = set()
                 for i_info in info['ipa']:
