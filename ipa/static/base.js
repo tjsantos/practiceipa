0e4542ad038a2d74440feab5549b496b5faea649
T.J. Santos <tjscode@outlook.com>
2015-03-11 03:37:01 -0700

    Add confirm dialog to reset progress

--- ipa/static/base.js
+++ ipa/static/base.js
@@ -1,11 +1,20 @@
 var audioControls = {
-  setup: function() {
-    var playButton = '<button class="btn btn-s play-audio">' +
-      '<span class="glyphicon glyphicon-volume-up"></span></button>';
-    $('audio').addClass('hide').after(playButton);
-    $('.play-audio').on('click', function() {
-      $(this).prev('audio').trigger('play');
-    });
-  }
+    setup: function() {
+        var playButton = '<button class="btn btn-s play-audio">' +
+            '<span class="glyphicon glyphicon-volume-up"></span></button>';
+        $('audio').addClass('hide').after(playButton);
+        $('.play-audio').on('click', function() {
+            $(this).prev('audio').trigger('play');
+        });
+    }
 };
 $(audioControls.setup);
+
+var resetProgress = {
+    setup: function() {
+        $('#reset-progress').on('click', function() {
+            return confirm('Reset progress?');
+        });
+    }
+};
+$(resetProgress.setup);
