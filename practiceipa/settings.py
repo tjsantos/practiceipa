40ccbfdd766de2f778dd7002ecb754db7ee6a7ab
TJ Santos <tjscode@outlook.com>
2016-03-24 23:08:23 -0700

    Update outdated packages

--- practiceipa/settings.py
+++ practiceipa/settings.py
@@ -139,7 +139,7 @@ LOGGING = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 
